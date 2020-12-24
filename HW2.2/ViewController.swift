ed997f0a6927eccb77ce51e4f16c44159e906655
Iushko Pavel <rig.86@mail.ru>
2020-12-24 23:55:08 +0300

    фиксирую ширину лейбла

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var changeView: UIImageView!
+    @IBOutlet var redLabelLeft: UILabel!
+    @IBOutlet var greenLabelLeft: UILabel!
+    @IBOutlet var blueLabelLeft: UILabel!
+    @IBOutlet var redLabelRight: UILabel!
+    @IBOutlet var greebLabelRight: UILabel!
+    @IBOutlet var blueLabelRight: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        changeView.layer.cornerRadius = 20
+        redLabelRight.backgroundColor = .brown
+
     }
 
-
+    @IBAction func valueRed() {
+    }
+    @IBAction func valueGreen() {
+    }
+    @IBAction func valueBlue() {
+    }
+    
+    
 }
 
