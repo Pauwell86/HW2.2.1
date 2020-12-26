2a6da77202795e762e1ec612ffbc4b28e80b6522
Iushko Pavel <rig.86@mail.ru>
2020-12-26 12:21:48 +0300

    corner done

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -14,13 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet var greenLabelLeft: UILabel!
     @IBOutlet var blueLabelLeft: UILabel!
     @IBOutlet var redLabelRight: UILabel!
-    @IBOutlet var greebLabelRight: UILabel!
+    @IBOutlet var greenLabelRight: UILabel!
     @IBOutlet var blueLabelRight: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         changeView.layer.cornerRadius = 20
-        redLabelRight.backgroundColor = .brown
+        redLabelRight.backgroundColor = .white
+
+        redLabelRight.layer.masksToBounds = true
+        redLabelRight.layer.cornerRadius = 5
+        
+        greenLabelRight.layer.masksToBounds = true
+        greenLabelRight.layer.cornerRadius = 5
+        
+        blueLabelRight.layer.masksToBounds = true
+        blueLabelRight.layer.cornerRadius = 5
+ 
+     
 
     }
 
